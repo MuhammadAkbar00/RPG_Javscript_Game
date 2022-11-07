47726394718af40dd42bb18963bdb7cadfc6cdfd
abdvlla <117389248+abdvlla@users.noreply.github.com>
2022-11-07 01:27:58 -0400

    Create specialBoosts.js

---
+++ js/specialBoosts.js
@@ -0,0 +1,4 @@
+// Players can purchase special boosts for a temporary duration which allows them to double their earnings
+// For instance, if a player has a "Gold Boost (1 GAME)" on, then they will earn double the gold when they win
+// Unfortunately, I am still learning how to do shit so idk how to initiate it yet
+// May Allah send you to heaven
